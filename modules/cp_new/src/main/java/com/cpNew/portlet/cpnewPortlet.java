be4b8453fc19aab0b9aa5cabd3a4fbdd7309ce6e
HuyBamboo <huypham97bm@gmail.com>
2021-11-19 15:13:00 +0700

    update cp_new

--- modules/cp_new/src/main/java/com/cpNew/portlet/cpnewPortlet.java
+++ modules/cp_new/src/main/java/com/cpNew/portlet/cpnewPortlet.java
@@ -5,21 +5,20 @@ import com.liferay.asset.entry.rel.model.AssetEntryAssetCategoryRel;
 import com.liferay.asset.entry.rel.service.AssetEntryAssetCategoryRelLocalServiceUtil;
 import com.liferay.asset.kernel.model.AssetCategory;
 import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.asset.kernel.model.AssetLink;
 import com.liferay.asset.kernel.service.AssetCategoryLocalServiceUtil;
 import com.liferay.asset.kernel.service.AssetEntryLocalServiceUtil;
-import com.liferay.blogs.model.BlogsEntry;
-import com.liferay.blogs.service.BlogsEntryLocalServiceUtil;
+import com.liferay.asset.kernel.service.AssetLinkLocalServiceUtil;
 import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.service.DLAppLocalServiceUtil;
 import com.liferay.document.library.kernel.service.DLFileEntryLocalServiceUtil;
-import com.liferay.portal.kernel.dao.orm.DynamicQuery;
-import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
-import com.liferay.portal.kernel.dao.orm.Property;
-import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.repository.model.FileEntry;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -40,110 +39,73 @@ import org.osgi.service.component.annotations.Component;
 		"javax.portlet.resource-bundle=content.Language",
 		"javax.portlet.security-role-ref=power-user,user" }, service = Portlet.class)
 public class cpnewPortlet extends MVCPortlet {
-	
+
 	private final String LINK_CP_NEW = "http://portal.lifetek.vn/web/lifetek/media.chinhphu?id=";
-	@SuppressWarnings("deprecation")
+
 	@Override
 	public void doView(RenderRequest renderRequest, RenderResponse renderResponse)
 			throws IOException, PortletException {
 		try {
-			AssetCategory assetCategory1 = AssetCategoryLocalServiceUtil.getAssetCategory(72134);
-			renderRequest.setAttribute("assetCategory1", assetCategory1);
-			AssetCategory assetCategory2 = AssetCategoryLocalServiceUtil.getAssetCategory(71438);
-			renderRequest.setAttribute("assetCategory2", assetCategory2);
 			// img Banr tin chinh phu
 			renderRequest.setAttribute("srcImgBtcptq",
 					"http://daphuongtien.chinhphu.vn/file?path=/content/data/random/102021/CPTQ%20ava_9c38564d_bae176d1.jpg");
-
-			BlogsEntry blogTest = BlogsEntryLocalServiceUtil.getBlogsEntry(140411);
-			renderRequest.setAttribute("testImg", blogTest.getSmallImageFileEntryId());
 			// img đầu dòng các mục
 			String srcImgdaudong = "http://chinhphu.vn/templates/govportal/chinhphu/images/icon3.jpg";
 			renderRequest.setAttribute("srcImgdaudong", srcImgdaudong);
 
-			// danh sach top 5 muc Thông tin đa phương tiện
-			// lay danh sach blogs theo categoryId
-			List<AssetEntryAssetCategoryRel> assetCategoryRels = AssetEntryAssetCategoryRelLocalServiceUtil
-					.getAssetEntryAssetCategoryRelsByAssetCategoryId(72134);
-			List<BlogsEntry> blogsEntries1 = new ArrayList<>();
-			for (AssetEntryAssetCategoryRel a : assetCategoryRels) {
-				// lay assetEntryId trong AssetEntryAssetCategoryRel
-				long entryId = a.getAssetEntryId();
-				// lay assetEntry theo id vua lay
-				AssetEntry assetEntry = AssetEntryLocalServiceUtil.getAssetEntry(entryId);
-				// lay classPk cua AssetEntry
-				long blogsentryId = assetEntry.getClassPK();
-				// lay BlogsEntryId theo classPk
-				if (blogsentryId >= 1000) {
-					BlogsEntry blogsEntry = BlogsEntryLocalServiceUtil.getBlogsEntry(blogsentryId);
-					blogsEntries1.add(blogsEntry);
+			// categoryId Thông tin đa phương tiện - Bản tin chính phủ tuần qua
+			List<Integer> cateIds = Arrays.asList(72134, 71438);
+			int i = 0;
+			for (Integer ca : cateIds) {
+				i++;
+				AssetCategory assetCategory = AssetCategoryLocalServiceUtil.getAssetCategory(ca);
+				renderRequest.setAttribute("category" + i, assetCategory);
+				List<AssetEntryAssetCategoryRel> assetCategoryRels = AssetEntryAssetCategoryRelLocalServiceUtil
+						.getAssetEntryAssetCategoryRelsByAssetCategoryId(ca);
+
+				List<DLFileEntry> fileEntries = new ArrayList<>();
+				for (AssetEntryAssetCategoryRel a : assetCategoryRels) {
+					long entryId = a.getAssetEntryId();
+					AssetEntry assetEntry = AssetEntryLocalServiceUtil.getAssetEntry(entryId);
+					// check file is video???
+					if (assetEntry.getMimeType().equals("video/mp4")) {
+						// lấy fileId để lấy thông tin DLFileEntry(FileEntry)
+						long fileId = assetEntry.getClassPK();
+						DLFileEntry fileEntry = DLFileEntryLocalServiceUtil.getDLFileEntry(fileId);
+						// add FileEntry vao List
+						fileEntries.add(fileEntry);
+					}
 				}
-			}
-			// BlogsEntry1-5 theo blogsEntries giới hạn 5 phần tử và mới nhất
-			// Thông tin đa phương tiện(ttdpt)
-			List<BlogsEntry> blogsEntriesnew1 = blogsEntries1.stream().sorted().limit(5).collect(Collectors.toList());
-			for (int i = 0; i < blogsEntriesnew1.size(); i++) {
-				renderRequest.setAttribute("ttdpt" + (1 + i), blogsEntriesnew1.get(i));
+				List<DLFileEntry> fileEntriesNew = fileEntries.stream()
+						.sorted((s1, s2) -> (int) s2.getFileEntryId() - (int) s1.getFileEntryId()).limit(5)
+						.collect(Collectors.toList());
+				// lấy link ảnh cho mục đầu tiên
+				DLFileEntry mucdau = fileEntriesNew.get(0);
+				renderRequest.setAttribute("mucdau" + i, mucdau);
 
-				DynamicQuery query = DynamicQueryFactoryUtil.forClass(DLFileEntry.class);
-				Property propertiyFileName = PropertyFactoryUtil.forName("fileName");
-				String titltBlog = blogsEntriesnew1.get(i).getTitle().replaceAll("/", "-");
-				query.add(propertiyFileName.like(titltBlog + "%"));
-
-				List<DLFileEntry> dlFileEntries = DLFileEntryLocalServiceUtil.dynamicQuery(query);
-				for (DLFileEntry d : dlFileEntries) {
-					renderRequest.setAttribute("hrefttdpt" + (i + 1),
-							LINK_CP_NEW + d.getFileEntryId());
+				for (AssetEntryAssetCategoryRel a2 : assetCategoryRels) {
+					long entryId2 = a2.getAssetEntryId();
+					AssetEntry assetEntry2 = AssetEntryLocalServiceUtil.getAssetEntry(entryId2);
+					if (assetEntry2.getClassPK() == mucdau.getFileEntryId()) {
+						AssetLink assetLink = AssetLinkLocalServiceUtil.getLinks(entryId2).get(0);
+						if (assetLink.getEntryId1() == entryId2) {
+							AssetEntry asAnh = AssetEntryLocalServiceUtil.getAssetEntry(assetLink.getEntryId2());
+							FileEntry dlAnh = DLAppLocalServiceUtil.getFileEntry(asAnh.getClassPK());
+							renderRequest.setAttribute("srcAnh" + i, "/documents/" + dlAnh.getGroupId() + "/"
+									+ dlAnh.getFolderId() + "/" + dlAnh.getTitle() + "/" + dlAnh.getUuid());
+						}
+						if (assetLink.getEntryId2() == entryId2) {
+							AssetEntry asAnh = AssetEntryLocalServiceUtil.getAssetEntry(assetLink.getEntryId1());
+							FileEntry dlAnh = DLAppLocalServiceUtil.getFileEntry(asAnh.getClassPK());
+							renderRequest.setAttribute("srcAnh" + i, "/documents/" + dlAnh.getGroupId() + "/"
+									+ dlAnh.getFolderId() + "/" + dlAnh.getTitle() + "/" + dlAnh.getUuid());
+						}
+					}
 				}
+				fileEntriesNew.remove(mucdau);
+				renderRequest.setAttribute("fileEntriesNew" + i, fileEntriesNew);
 			}
-
-			// image tin thứ nhất của Thông tin đa phương tiện(ttdpt)
-			long fileEntryIdttdpt = blogsEntriesnew1.get(0).getSmallImageFileEntryId();
-			// tạo src của ảnh theo thông tin trong database
-			DLFileEntry imageTop1Ttdpt = DLFileEntryLocalServiceUtil.getDLFileEntry(fileEntryIdttdpt);
-			renderRequest.setAttribute("imgSrcTtdptTop1", "/documents/" + imageTop1Ttdpt.getGroupId() + "/" + imageTop1Ttdpt.getFolderId()
-					+ "/" + imageTop1Ttdpt.getTitle() + "/" + imageTop1Ttdpt.getUuid());
-//=====================================================================================
-			// danh sach top 5 muc Bản tin chính phủ tuần qua
-			// lay danh sach blogs theo categoryId
-			List<AssetEntryAssetCategoryRel> assetCategoryRels2 = AssetEntryAssetCategoryRelLocalServiceUtil
-					.getAssetEntryAssetCategoryRelsByAssetCategoryId(71438);
-			List<BlogsEntry> blogsEntries2 = new ArrayList<>();
-			for (AssetEntryAssetCategoryRel a : assetCategoryRels2) {
-				// lay assetEntryId trong AssetEntryAssetCategoryRel
-				long entryId = a.getAssetEntryId();
-				// lay assetEntry theo id vua lay
-				AssetEntry assetEntry = AssetEntryLocalServiceUtil.getAssetEntry(entryId);
-				// lay classPk cua AssetEntry
-				long blogsentryId = assetEntry.getClassPK();
-				// lay BlogsEntryId theo classPk
-				BlogsEntry blogsEntry = BlogsEntryLocalServiceUtil.getBlogsEntry(blogsentryId);
-				blogsEntries2.add(blogsEntry);
-
-			}
-			// BlogsEntry1-5 theo blogsEntries giới hạn 5 phần tử và mới nhất
-			// Bản tin chính phủ tuần qua(btcptq)
-			List<BlogsEntry> blogsEntriesnew2 = blogsEntries2.stream().sorted().limit(5).collect(Collectors.toList());
-			for (int i = 0; i < blogsEntriesnew2.size(); i++) {
-				renderRequest.setAttribute("btcptq" + (1 + i), blogsEntriesnew2.get(i));
-
-				DynamicQuery query2 = DynamicQueryFactoryUtil.forClass(DLFileEntry.class);
-				Property propertiyFileName2 = PropertyFactoryUtil.forName("fileName");
-				String titltBlog2 = blogsEntriesnew2.get(i).getTitle().replaceAll("/", "-");
-				query2.add(propertiyFileName2.like(titltBlog2 + "%"));
-				List<DLFileEntry> dlFileEntries2 = DLFileEntryLocalServiceUtil.dynamicQuery(query2);
-				for (DLFileEntry d : dlFileEntries2) {
-					renderRequest.setAttribute("hrefbtcptq" + (i + 1),
-							LINK_CP_NEW + d.getFileEntryId());
-				}
-			}
-			// image tin thứ nhất của Bản tin chính phủ tuần qua (btcptq)
-			long fileEntryIdBtcptq = blogsEntriesnew2.get(0).getSmallImageFileEntryId();
-			// tạo src của ảnh theo thông tin trong database
-			DLFileEntry imageTop1Btcptq = DLFileEntryLocalServiceUtil.getDLFileEntry(fileEntryIdBtcptq);
-			renderRequest.setAttribute("imgSrcBtcptqTop1", "/documents/" + imageTop1Btcptq.getGroupId() + "/" + imageTop1Btcptq.getFolderId()
-					+ "/" + imageTop1Btcptq.getTitle() + "/" + imageTop1Btcptq.getUuid());
-
+			renderRequest.setAttribute("LINK_CP_NEW", LINK_CP_NEW);
 		} catch (PortalException e) {
 			e.printStackTrace();
 		}
